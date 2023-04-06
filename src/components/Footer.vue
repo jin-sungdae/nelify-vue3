87d28c71aa360beccb5580dfdb7aa1229c06ff7a
jinsungdae <sjin@ninetree.com>
2023-04-06 17:01:45 +0900

    init

---
+++ src/components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+  <footer>
+    <Logo />
+    <a href="https://github.com/ParkYoungWooong"
+    target="_black">
+    (c){{ new Date().getFullYear() }}Sungdae</a>
+  </footer>  
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+
+  }
+}
+</style>
---
+++ src/store/about.js
@@ -0,0 +1,11 @@
+export default {
+    namespaced: true,
+    state: () => ({
+        name: 'Sungdae',
+        email: 'wlstjdeo00@gmail.com',
+        blog: 'https://velog.io/sjin',
+        phone: '+82-10-9771-5131',
+        image: 'https://heropy.blog/css/images/logo.png'
+    })
+
+}
